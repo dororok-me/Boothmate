4ce34bba1c5addae48cf7f43e237e6a493a84747
dororok <dororok@gmail.com>
2026-03-26 22:35:37 +0900

    Initial Commit

---
+++ Boothmate/BoothmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoothmateApp.swift
+//  Boothmate
+//
+//  Created by dororok on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BoothmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
